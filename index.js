83f7452fc68167ece96a7bad644c46e2f79726d3
Piero Nicolini <naturalnen@gmail.com>
2024-12-17 23:28:28 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+let cantidadSpan = document.querySelector('.cantidad')
+let precioSpan = document.querySelector(".precio-inicial");
+let cantidad = 0;
+const precio = 400000;
+
+precioSpan.innerHTML = precio;
+
+let precioTotal = document.querySelector(".valor-total");
